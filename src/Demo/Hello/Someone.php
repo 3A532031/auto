e83957cac953e14873c61b942bc3b494239780ea
Ray-PC\Ray <quzz96er43@mail.com>
2018-10-20 20:23:33 +0800

    3.3

---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,17 @@
+<?php
+namespace Demo\Hello;
+
+class Someone
+{
+    function __construct($name)
+    {
+        echo 'Hello '.$name.PHP_EOL;
+    }
+}
+
+/**
+ * Created by PhpStorm.
+ * User: Ray
+ * Date: 2018/10/20
+ * Time: 下午 08:23
+ */
