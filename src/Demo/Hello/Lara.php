f9438ef8bd9553315e035208c4803aef399d7e96
Ray-PC\Ray <quzz96er43@mail.com>
2018-10-20 20:22:53 +0800

    3.2

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,17 @@
+<?php
+namespace Demo\Hello;
+
+class Lara
+{
+    function __construct()
+    {
+        echo 'Hello Lara~'.PHP_EOL;
+    }
+}
+
+/**
+ * Created by PhpStorm.
+ * User: Ray
+ * Date: 2018/10/20
+ * Time: 下午 08:22
+ */
