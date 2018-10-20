6e129f2ae7936e64ce2574af181d0bad0bc25f93
Ray-PC\Ray <quzz96er43@mail.com>
2018-10-20 20:21:38 +0800

    3.1

---
+++ src/Demo/HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+namespace Demo;
+
+class HelloWorld
+{
+    function __construct()
+    {
+        echo 'Hello World ~'.PHP_EOL;
+    }
+}
+
+/**
+ * Created by PhpStorm.
+ * User: Ray
+ * Date: 2018/10/20
+ * Time: 下午 08:21
+ */
